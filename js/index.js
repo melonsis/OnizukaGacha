b4045cf45a1d23647585f171b25b307da4b35690
unknown <tch1995@live.com>
2024-11-28 20:12:38 +0800

    Try to fix bug at table 3

--- js/index.js
+++ js/index.js
@@ -329,7 +329,7 @@ $(function() {
         i = 1;
         while (i <= times) {
             rs = modi_action(poolname);
-            if ($('#pool1').style("color") == "#FFFFFF") {
+            if (poolname == 'A') {
                 AtbMain = "#CAtbMain";
                 //R5tbMain = "#CR5tbMain";
                 //R4tbMain = "#CR4tbMain";
